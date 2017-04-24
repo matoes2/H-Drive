01660b0606daf29025097eefa3336cc3cc8e342c
Alejandro Rodriguez <arodriguez@students.inghamisd.org>
2017-04-24 11:55:52 -0400

    Set up initial repository

--- .bash_history
+++ .bash_history
@@ -12,3 +12,14 @@ git add.
 git commit -m "First Commit"
 git add .
 git commit -m "First Commit"
+git init
+pwd
+git add .
+git commit -m "First Commit"
+git config --global -- edit
+git config --global --edit
+git commit --amend --reset-author
+git remote add origin https://github.com/matoes2/H-Drive.git
+git remote -v
+git push origin master
+git push --set-upstream origin master
---
+++ Object Oriented Programming/ReadingAndWriting/ReadingAndWriting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadingAndWriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(StreamWriter sw = new StreamWriter("n00bz.txt"))
+            {
+                sw.WriteLine("Hello World");
+                sw.WriteLine("Pussy Lover!!");
+                sw.WriteLine("Hello World");
+                sw.WriteLine("Pussy Lover!!");
+                sw.WriteLine("Hello World");
+                sw.WriteLine("Pussy Lover!!");
+                sw.WriteLine("Hello World");
+                sw.WriteLine("Pussy Lover!!");
+                sw.WriteLine("Hello World");
+                sw.WriteLine("Pussy Lover!!");
+            }
+            using (StreamReader sr = new StreamReader("n00bz.txt"))
+            {
+                string name = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine() + name);
+                }
+            }
+        }
+    }
+}
